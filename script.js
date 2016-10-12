546664036b0747c3695d11f3db65346ace2477ca
Slepzs <slepzs@gmail.com>
2016-10-12 12:50:26 +0200

    Index FIl

---
+++ script.js
@@ -0,0 +1,21 @@
+var blue = document.getElementById('blue');
+var red = document.getElementById('red');
+
+blue.onclick = function(p1) {
+    blue.style.backgroundColor = "blue";
+}
+red.onclick = function(p2) {
+    red.style.backgroundColor = "red";
+}
+
+console.log("Det virker");
+
+
+function cos() {
+    var image = document.getElementById("green");
+    if (backgroundColor = "green" > backgroundColor = "yellow") {
+        image.style.backgroundColor = "green";
+    } else {
+        image.style.backgroundColor = "yellow";
+    }
+}
