3a3babdd1c7bea5d359d121e4354580bdc24889d
Slepzs <slepzs@gmail.com>
2016-10-27 23:48:16 +0200

    Revert "Kasser - og tekst og responsive"
    This reverts commit ebeb8d305836ef298d30608dcd41ca9c07cf83c8.

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
- var slideIndex = 0;
+var slideIndex = 0;
 showSlides();
 
- function showSlides() {
- var i;
+function showSlides() {
+    var i;
     var slides = document.getElementsByClassName("mySlides");
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none"; 
     }
     slideIndex++;
     if (slideIndex> slides.length) {slideIndex = 1} 
-    slides[slideIndex-1].style.display = "block";     
-     setTimeout(showSlides, 4000); // Skifter billede hvert 4 sekund
-} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 4000); // Skifter billede hvert 4 sekund
+}
